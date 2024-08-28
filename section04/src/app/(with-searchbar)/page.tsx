205931a8ca30b1c270ed55b90071b5e42811d2d4
Sangwoo <jswoo1995@gmail.com>
2024-08-28 15:26:46 +0900

    [feat] section04

---
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,50 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+    const allBooks: BookData[] = await res.json();
+    // console.log(allBooks);
+
+    if (!res.ok) {
+        return <div>오류가 발생하였습니다.</div>;
+    }
+
+    return (
+        <div>
+            {allBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
+
+async function RecoBooks() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+    const RecoBooks: BookData[] = await res.json();
+    console.log(RecoBooks);
+
+    return (
+        <div>
+            {RecoBooks.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
+}
+
+export default function Home() {
+    return (
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                <RecoBooks />
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                <AllBooks />
+            </section>
+        </div>
+    );
+}
